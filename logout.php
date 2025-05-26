73798d9dacac14191d1a779cacf22e566a3b2bbb
Sabasorourian <103371459+sabasorourian@users.noreply.github.com>
2025-05-26 12:20:29 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,26 @@
 <?php
 session_start();
+
+// Unset all session variables
+$_SESSION = [];
+
+// If you’re using cookies for sessions, expire that cookie too:
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),    // usually PHPSESSID
+        '',
+        time() - 42000,    // in the past
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Finally destroy the session on the server
 session_destroy();
-header("Location: login.php");
-?>
+
+// Send the user back to login
+header('Location: login.php');
+exit;
